2ffddc6bcf677d88ce402e5dabc578f849bc7586
chrisstruong <chrisstruong@gmail.com>
2023-05-27 15:51:38 -0400

    Work on react to build monoco code editor

--- code-compiler/src/App.js
+++ code-compiler/src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css"
+import Editor from "@monaco-editor/react";
+import Navbar from "./Components/Navbar"
+import Axios from "axios";
+import spinner from "./spinner.svg"
 
 function App() {
+  // State variable to set users source code
+  const [userCode, setUserCode] = useState(``)
+
+  // State variable to set editors default language
+  const [userLang, setUserLang] = useState("python")
+
+  // State variable to set editors default theme
+  const [userTheme, setUserTheme] = useState("vs-dark")
+
+  // State variable to set editors default font size
+  const [fontSize, setFontSize] = useState(20)
+
+  // State variable to set users input
+  const [userInput, setUserInput] = useState("")
+
+  // State variable to set users output
+  const [userOutput, setUserOutput] = useState("")
+
+  // Loading state variable to show spinner
+  // While fetching data
+
+  const [loading, setLoading] = useState(false)
+
+  const options = {
+    fontSize: fontSize
+  }
+
+  // Function to call the compule endpoint
+  function compile() {
+    setLoading(true)
+    if (userCode === ``) {
+      return
+    }
+
+    // Post request to compile endpoint
+    Axios.post(`http://localhost:8000/compile`, {
+      code: userCode,
+      language: userLang,
+      input: userInput
+    }).then((res) => {
+      setUserOutput(res.data.output)
+    }).then(() => {
+      setLoading(false)
+    })
+  }
+
+  // Function to clear the output screen
+  function clearOutput() {
+    setUserOutput("")
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar
+        userLang={userLang} setUserLang={setUserLang}
+        userTheme={userTheme} setUserTheme={setUserTheme}
+        fontSize={fontSize} setFontSize={setFontSize}
+      />
+      <div className="main">
+        <div className="left-container">
+          <Editor
+          options={options}
+          height="calc(100vh - 50px)"
+          width="100%"
+          theme={userTheme}
+
+        </div>
+
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
