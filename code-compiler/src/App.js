ab4f66404c79f6bbb839043a35ace29f7e650d34
chrisstruong <chrisstruong@gmail.com>
2023-06-13 12:01:57 -0400

    Try to use Monaco Editor

--- code-compiler/src/App.js
+++ code-compiler/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import Editor, { useMonaco } from "@monaco-editor/react";
+import Editor from "@monaco-editor/react";
 import Navbar from './Components/Navbar';
 import Axios from 'axios';
 import spinner from './spin.jpeg';
@@ -34,7 +34,6 @@ function App() {
 		fontSize: fontSize
 	}
 
-	const monaco = useMonaco()
 
 	// Function to call the compile endpoint
 	function compile() {
@@ -60,58 +59,71 @@ function App() {
 		setUserOutput("");
 	}
 
-	
-	
+
+	const [content, setContent] = useState('')
 
 	return (
-		<div className="App">
-			<Navbar
-				userLang={userLang} setUserLang={setUserLang}
-				userTheme={userTheme} setUserTheme={setUserTheme}
-				fontSize={fontSize} setFontSize={setFontSize}
-			/>
-			<div className="main">
-				<div className="left-container"> 
-					<Editor
-			options={options}
-			height="calc(100vh - 50px)"
-			width="100%"
-			theme={userTheme}
-			language={userLang}
-			defaultLanguage="javascript"
-			defaultValue="// Enter your code here"
-			onChange={(value) => { setUserCode(value) }}
-		/>
-				<button className="run-btn" onClick={() => compile()}>
-						Run
-					</button>
-				</div>
-				<div className="right-container">
-					<h4>Input:</h4>
-					<div className="input-box">
-						<textarea id="code-inp" onChange=
-							{(e) => setUserInput(e.target.value)}>
-						</textarea>
-					</div>
-					<h4>Output:</h4>
-					{loading ? (
-						<div className="spinner-box">
-							<img src={spinner} alt="Loading..." />
-						</div>
-					) : (
-						<div className="output-box">
-							<pre>{userOutput}</pre>
-							<button onClick={() => { clearOutput() }}
-								className="clear-btn">
-								Clear
-							</button>
-						</div>
-					)}
-				</div>
 
-			</div>  
-			{/* <MonacoEditor /> */}
+
+
+		<div>
+			<Editor
+				height="100vh"
+				theme="vs-dark"
+				defaultLanguage='javascript'
+				value="//Enter the code"
+				 onChange={(value)=> setContent(value) }
+
+			/>
 		</div>
+		// <div className="App">
+		// 	<Navbar
+		// 		userLang={userLang} setUserLang={setUserLang}
+		// 		userTheme={userTheme} setUserTheme={setUserTheme}
+		// 		fontSize={fontSize} setFontSize={setFontSize}
+		// 	/>
+		// 	<div className="main">
+		// 		<div className="left-container">
+		// 			<Editor
+		// 				options={options}
+		// 				height="calc(100vh - 50px)"
+		// 				width="100%"
+		// 				theme={userTheme}
+		// 				language={userLang}
+		// 				defaultLanguage="javascript"
+		// 				defaultValue="// Enter your code here"
+		// 				onChange={(value) => { setUserCode(value) }}
+		// 			/>
+		// 			<button className="run-btn" onClick={() => compile()}>
+		// 				Run
+		// 			</button>
+		// 		</div>
+		// 		<div className="right-container">
+		// 			<h4>Input:</h4>
+		// 			<div className="input-box">
+		// 				<textarea id="code-inp" onChange=
+		// 					{(e) => setUserInput(e.target.value)}>
+		// 				</textarea>
+		// 			</div>
+		// 			<h4>Output:</h4>
+		// 			{loading ? (
+		// 				<div className="spinner-box">
+		// 					<img src={spinner} alt="Loading..." />
+		// 				</div>
+		// 			) : (
+		// 				<div className="output-box">
+		// 					<pre>{userOutput}</pre>
+		// 					<button onClick={() => { clearOutput() }}
+		// 						className="clear-btn">
+		// 						Clear
+		// 					</button>
+		// 				</div>
+		// 			)}
+		// 		</div>
+
+		// 	</div>
+			
+		// </div>
 	);
 }
 
