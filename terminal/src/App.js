aed17eac82e59212e6b0cd5f2da9133e17905622
chrisstruong <chrisstruong@gmail.com>
2023-06-18 13:57:00 -0400

    Finish the web-based terminal

--- terminal/src/App.js
+++ terminal/src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 function App() {
   const [input, setInput] = useState("")
   const [output, setOutput] = useState("")
+  const inputRef = useRef()
+
+  useEffect(() => {
+    inputRef.current.focus()
+  }, [])
+
+
   return (
-    <div className="App">
+    <div 
+    className="App"
+    onClick={e => inputRef.current.focus()}
+
+    >
+      
       <input
+        ref={inputRef}
         type="text"
         value={input}
         onChange={e => setInput(e.target.value)}
@@ -22,7 +35,7 @@ function App() {
                 newOutput += "Your on my cool site"
                 break
               default:
-                  newOutput += "Unknown Command"
+                newOutput += "Unknown Command"
             }
             setOutput(newOutput)
             setInput("")
