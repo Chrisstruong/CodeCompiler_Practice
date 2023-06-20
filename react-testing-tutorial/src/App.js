fe1a1a3deb101d4e6a8b9893dad4ab2090867466
chrisstruong <chrisstruong@gmail.com>
2023-06-20 15:34:43 -0400

    Fall in love working with this test file

--- react-testing-tutorial/src/App.js
+++ react-testing-tutorial/src/App.js
@@ -1,5 +1,4 @@
-import { render, fireEvent, screen} from "@testing-library/react"
-import Counter from "./Components/Counter";
+
 
 function App() {
   return (
--- react-testing-tutorial/src/Components/test.test.js
+++ react-testing-tutorial/src/Components/test.test.js
@@ -1,19 +1,18 @@
-import { render, fireEvent, screen} from "@testing-library/react"
-import Counter from "./Components/Counter";
+import { render, fireEvent, screen } from "@testing-library/react";
+import Counter from "./Counter";
 
 //test block
-test("increments counter" , () => {
-    // render the component on virtual dom
-    render(<Counter />)
-  
-  //select the elements you want to interact with
-  const counter = screen.getByTestId("counter")
-  const incrementBtn = screen.getByTestId("increment")
+test("increments counter", () => {
+// render the component on virtual dom
+render(<Counter />);
 
-  //interact with those elements
-  fireEvent.click(incrementBtn)
+//select the elements you want to interact with
+const counter = screen.getByTestId("counter");
+const incrementBtn = screen.getByTestId("increment");
 
-  //assert the expected result
-  expect(counter).toHaveTextContent("1")
-  
-  })
+//interact with those elements
+fireEvent.click(incrementBtn);
+
+//assert the expected result
+expect(counter).toHaveTextContent("1");
+});
