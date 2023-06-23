d7acb63f8ec3f5a0c416aa8385498a345095ea67
chrisstruong <chrisstruong@gmail.com>
2023-06-23 15:16:48 -0400

    Finish the first tutorial

--- Mocha/specs/myClas.spec.js
+++ Mocha/specs/myClas.spec.js
@@ -1,8 +1,13 @@
 const MyClass = require("../src/myClass.js")
 var myObj = new MyClass()
+var chai = require("chai")
+var expect = chai.expect
 
 describe("Test suit", function () {
     it("Test the add method", function () {
-        myObj.add(1, 2)
+        expect(myObj.add(1, 2)).to.be.equal(3)
+    })
+    it("Test the mostFrequentChar", function () {
+        expect(myObj.mostFrequentChar('bookeeper')).to.be.equal('e')
     })
 })
