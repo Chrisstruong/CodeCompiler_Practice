7a0c3e4b873eb33c6da948f5e0d7dea5c86d8213
chrisstruong <chrisstruong@gmail.com>
2023-07-03 16:08:29 -0400

    Finally create a response from mocha

--- online-compiler/backend/test.spec.js
+++ online-compiler/backend/test.spec.js
@@ -1,16 +1,19 @@
 const {mostFrequentChar} = require("./codes/mostFrequentChar123")
+const axios = require("axios")
 const chai = require("chai")
 const expect = chai.expect
 
 describe("test the mostfrequentChar", function() {
-    it("Test the mostFrequentChar method", function() {
-        const a = expect(mostFrequentChar('bookeeper')).to.be.equal('e')
-        console.log(a)
+    it("Test the mostFrequentChar method", async () =>  {
+        expect(mostFrequentChar('bookeeper')).to.be.equal('e')
+        const res = await axios.post("http://localhost:1000/mocha", {
+            "status": "True Success"
+        })
     })
     it("Test the mostFrequentChar method 2", function() {
         expect(mostFrequentChar('aaaaa')).to.be.equal('a')
     })
     it("Test the mostFrequentChar method 3", function() {
-        expect(mostFrequentChar('bbbb')).to.be.equal('e')
+        expect(mostFrequentChar('bbbb')).to.be.equal('b')
     })
 })
